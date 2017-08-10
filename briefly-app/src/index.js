d8e4a4278b7b54b21963090c91a59cc65ec6d0e2
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-08-10 16:34:40 -0700

    Better dispatch of contents

--- briefly-app/src/index.js
+++ briefly-app/src/index.js
@@ -3,35 +3,35 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-let rawContents = document.getElementById('input-document').value;
-let estimatedTime = document.getElementById('input-time').value;
-let reward = document.getElementById('input-reward').value;
+function loadPage(contents, estimatedTime, reward) {
+  ReactDOM.render(<App contents={contents} estimatedTime={estimatedTime} reward={reward} />, document.getElementById('root'));
+}
 
-let contents;
+const url = new URL(window.location);
+const estimatedTime = document.getElementById('input-time').value;
+const reward = document.getElementById('input-reward').value;
+const rawContents = document.getElementById('input-document').value;
 
-let url = new URL(window.location);
+// Better mechanism to select which to render.
 if (url.pathname.match(/\/([0-9])+/)) {
   let fname = url.pathname.match(/\/([0-9])+/)[1];
   axios.get("/data/pilot-out/" + fname + ".json")
     .then((result) => {
-      contents = result.data;
-      console.log(result.data);
-      ReactDOM.render(<App contents={contents} estimatedTime={estimatedTime} reward={reward} />, document.getElementById('root'));
-      registerServiceWorker();
+      let contents = result.data;
+      loadPage(contents, estimatedTime, reward);
     })
+} else if (rawContents.length > 0 && rawContents[0] !== '$') {
+  let contents = JSON.parse(rawContents);
+  loadPage(contents, estimatedTime, reward);
+} else {
+  let contents = {
+    "title": "Title",
+    "paragraphs": ["A line of text."],
+    "selections": [[], [[0,3, 1.0], [5, 7, 0.4]]]
+  };
+  loadPage(contents, estimatedTime, reward);
 }
-
-// if (rawContents.length > 0 && rawContents[0] !== '$') {
-//   contents = JSON.parse(rawContents);
-// } else {
-//   contents = {
-//     "title": "Title",
-//     "paragraphs": ["A line of text."],
-//     "selections": [[], [[0,3, 1.0], [5, 7, 0.4]]]
-//   };
-// }
 
 // Some hijinks for AMT
 if (document.getElementById('submitButton') !== null) {
