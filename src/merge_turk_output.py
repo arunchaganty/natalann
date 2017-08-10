5bfdb04e69706ee704e38bcba7efa3149c233bb3
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-08-10 15:28:02 -0700

    Useful turk tasks

---
+++ src/merge_turk_output.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Read a file from input and merge it in output.
+"""
+
+import pdb
+import os
+import csv
+import sys
+import json
+import html
+from collections import defaultdict
+
+def read(istream):
+    reader = csv.reader(istream, delimiter=',')
+    header = next(reader)
+    assert len(header) > 0, "Invalid header"
+    #Row = namedtuple('Row', header)
+    #return (Row(*row) for row in reader)
+    return (dict(zip(header, row)) for row in reader)
+
+def merge_(spans):
+    max_weight = 0.8
+    max_count = len(spans)
+    spans = sorted(sum(spans, []))
+    if len(spans) == 0:
+        return []
+    print(spans)
+
+    ret = []
+    start, end = spans.pop(0)
+    count = 1
+    #pdb.set_trace()
+    while len(spans) > 0:
+        print(start, end, spans, ret)
+        start_, end_ = spans.pop(0)
+        if end <= start_: # Finish before x does.
+            ret.append([start, end, count * max_weight / max_count])
+            count -= 1
+            start, end = start_, end_
+        else: # There is some overlap.
+            # Break the current spans into [start, start_], [start_, min(end, end_)], [min(end, end_), max(end, end_)]
+            ret.append([start, start_, count * max_weight / max_count])
+            spans = sorted(spans + [[min(end, end_), max(end, end_)]])
+            count += 1
+            start, end = start_, min(end, end_)
+
+    ret.append([start, end, count * max_weight / max_count])
+
+    return ret
+
+def test_merge_():
+    spans = [
+        [],
+        [(1,10)],
+        [(2,7), (9, 11)],
+        ]
+    gold = [
+        (1, 2, 0.8*1/3),
+        (2, 7, 0.8*2/3),
+        (7, 9, 0.8*1/3),
+        (9, 10, 0.8*2/3),
+        (10, 11, 0.8*1/3),
+        ]
+
+    ret = merge_(spans)
+    assert ret == gold
+
+def merge(outputs):
+    return [merge_(section) for section in zip(*outputs)]
+
+def do_command(args):
+    data = defaultdict(list)
+
+    for row in read(args.input):
+        doc_raw = html.unescape(row["Input.document"])
+        selections_raw = html.unescape(row["Answer.selections"])
+        selections = json.loads(selections_raw)
+
+        data[doc_raw].append(selections)
+
+    for i, (doc_raw, outputs) in enumerate(data.items()):
+        doc = json.loads(doc_raw)
+        doc["selections"] = merge(outputs) #outputs[0] # 
+
+        with open(os.path.join(args.output, "{}.json".format(i)), "w") as f:
+            json.dump(doc, f)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin, help="CSV file containing judgements.")
+    parser.add_argument('-o', '--output', type=str, default="../data/pilot-out/", help="Where to write output files")
+    parser.set_defaults(func=do_command)
+
+    ARGS = parser.parse_args()
+    if ARGS.func is None:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        ARGS.func(ARGS)
---
+++ src/prepare_turk_batch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import os
+import sys
+import csv
+import json
+import html
+
+def do_command(args):
+    assert os.path.exists(args.input)
+
+    writer = csv.writer(args.output)
+    writer.writerow(["document"])
+
+    for fname in os.listdir(args.input):
+        if not fname.endswith('.json'): continue
+        with open(os.path.join(args.input, fname)) as f:
+            doc = json.load(f)
+            writer.writerow([html.escape(json.dumps(doc))])
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-i', '--input', type=str, default='../data/pilot', help="Directory with JSON files")
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help="A CSV to use with MTurk")
+    parser.set_defaults(func=do_command)
+
+    #subparsers = parser.add_subparsers()
+    #command_parser = subparsers.add_parser('command', help='' )
+    #command_parser.set_defaults(func=do_command)
+
+    ARGS = parser.parse_args()
+    if ARGS.func is None:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        ARGS.func(ARGS)
