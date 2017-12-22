c2cc1b6b8c396cf9f56a990ab668d4733a3ba13b
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-12-22 15:09:06 -0800

    Added new segmentlist datastructure

---
+++ briefly-app/src/SegmentList.js
@@ -0,0 +1,92 @@
+/**
+ * Defines a segment data structure.
+ */
+import update from 'immutability-helper';
+
+// Each segment is simply a tuple of (start position), (end position)
+class SegmentList {}
+
+SegmentList.isValidSegment = function(sel) {
+  return sel[0] < sel[1];
+}
+
+SegmentList.merge = function(a, b) {
+  let ret = [];
+  let ai = 0;
+  let bi = 0;
+  while (ai < a.length && bi < b.length) {
+    if (a[ai] < b[bi]) {
+      ret.push(a[ai++]);
+    } else {
+      ret.push(b[bi++]);
+    }
+  }
+  while (ai < a.length) {
+    ret.push(a[ai++]);
+  }
+  while (bi < b.length) {
+    ret.push(b[bi++]);
+  }
+
+  return ret;
+};
+
+SegmentList.insert = function(lst, sel) {
+  console.assert(SegmentList.isValidSegment(sel));
+
+  if (lst.length === 0) {
+    return [sel];
+  }
+
+  // We now are dealing with non-empty lists.
+  // Iterate through the list to find the segment we're at.
+  for (let i = 0; i < lst.length; i++) {
+    const sel_ = lst[i];
+    if (sel[1] < sel_[0]) { // Aha, this is right before sel_.
+      return update(lst, {$splice: [[i, 0, sel]]});
+    } else if (sel[0] < sel_[1]) { // Hrm... this intersects with sel_, we need to split it up.
+      let newSel = [Math.min(sel[0], sel_[0]), Math.max(sel[1], sel_[1])]
+      return update(lst, {$splice: [[i, 1, newSel]]});
+    }
+  }
+  return update(lst, {$push: [sel]});
+};
+
+SegmentList.remove = function(lst, sel) {
+  console.assert(SegmentList.isValidSegment(sel));
+
+  if (lst.length === 0) {
+    return [];
+  }
+
+  // We now are dealing with non-empty lists.
+  // Iterate through the list to find the segment we're at.
+  for (let i = 0; i < lst.length;) {
+    const sel_ = lst[i];
+    if (sel[0] > sel_[1]) { i++; continue;} // this segment is before our removal segment
+    else if (sel[1] <= sel_[0]) break; // ok, the removal segment is outside our range, we can stop.
+
+    // At this point there is definitely some overlap.
+
+    // If removal is before this, just forward the segment.
+    let splice = [i, 1];
+    if (sel[0] > sel_[0]) {
+      splice.push([sel_[0], sel[0]]);
+      i++;
+    }
+    if (sel[1] < sel_[1]) {
+      splice.push([sel[1], sel_[1]]);
+      i++;
+    } 
+    lst = update(lst, {$splice: [splice]});
+    
+    if (sel_[1] < sel[1]) {
+      sel = [sel[1], sel_[1]];
+    } else {
+      break;
+    }
+  }
+  return lst;
+};
+
+export default SegmentList;
---
+++ briefly-app/src/SegmentList.test.js
@@ -0,0 +1,25 @@
+import SegmentList from './SegmentList';
+
+it('returns isValid', () => {
+  expect(SegmentList.isValidSegment([1, 2]), true);
+  expect(SegmentList.isValidSegment([3, 2]), false);
+  expect(SegmentList.isValidSegment([2, 2]), false);
+});
+
+it('returns insertedList', () => {
+  const lst = [[1,10], [14, 17]];
+  expect(SegmentList.insert(lst, [3, 7]), lst);
+  expect(SegmentList.insert(lst, [11, 12]), [[1,10], [11,12], [14,17]]);
+  expect(SegmentList.insert(lst, [7, 12]), [[1,12], [14,17]]);
+  expect(SegmentList.insert(lst, [20, 22]),  [[1,10], [14,17], [20, 22]]);
+});
+
+it('returns removedList', () => {
+  const lst = [[1,10], [14, 17]];
+  expect(SegmentList.remove([], [3,7]), []);
+  expect(SegmentList.remove(lst, [1,10]), [[14, 17]]);
+  expect(SegmentList.remove(lst, [1,7]), [[7,10], [14, 17]]);
+  expect(SegmentList.remove(lst, [3,7]), [[1,3], [7,10], [14, 17]]);
+  expect(SegmentList.remove(lst, [1,12]), [[14, 17]]);
+  expect(SegmentList.remove(lst, [7,16]), [[1,7], [16,17]]);
+});
